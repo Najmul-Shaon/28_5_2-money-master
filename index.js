07e787ff0c2fea2bd2186d6a7871fd7d77cf3fd5
Najmul-Shaon <najmul.nh.shaon@gmail.com>
2022-10-02 22:17:17 +0600

    all finish

--- index.js
+++ index.js
@@ -3,24 +3,63 @@ document.getElementById('expense-calculate').addEventListener('click', function 
     const rentCostElement = document.getElementById('rent-cost');
     const clothsCostElement = document.getElementById('cloths-cost');
     const totalExpenseElement = document.getElementById('total-expense');
+    const totalIncomeElement = document.getElementById('income-field');
+    const balanceElement = document.getElementById('balance');
 
     const foodCostString = foodCostElement.value;
     const rentCostString = rentCostElement.value;
     const clothsCostString = clothsCostElement.value;
+    const incomeString = totalIncomeElement.value;
 
     const foodCost = parseInt(foodCostString);
     const rentCost = parseInt(rentCostString);
     const clothsCost = parseInt(clothsCostString);
-
+    const income = parseInt(incomeString);
     const totalExpense = foodCost + rentCost + clothsCost;
 
     totalExpenseElement.innerText = totalExpense;
 
     // calculate balance 
 
+    const newBalance = income - totalExpense;
+
+    balanceElement.innerText = newBalance;
+})
+
+document.getElementById('save').addEventListener('click', function () {
+    const totalIncomeElement = document.getElementById('income-field');
+    const incomeString = totalIncomeElement.value;
+    const income = parseInt(incomeString);
+
+    const saveValueElement = document.getElementById('save-value');
+    const saveString = saveValueElement.value;
+    const save = parseFloat(saveString);
+
+    const saveAmount = (income * save) / 100;
+
+
+
+    const savingAmountElement = document.getElementById('saving-amount');
+    savingAmountElement.value = saveAmount;
+
+    const balanceElement = document.getElementById('balance');
+    const balanceString = balanceElement.innerHTML;
+    const newBalance = parseInt(balanceString);
+
+    const remainingBalance = newBalance - saveAmount;
+
+    const remainBalanceElement = document.getElementById('remain-balance');
+
+    remainBalanceElement.value = remainingBalance;
 
 
 
 
-    console.log(totalExpense);
+
+
+
+
+    // console.log(income);
+    // console.log(save);
+
 })
