d6ec77fb25d40c851162712bc9d25127302f6db3
Najmul-Shaon <najmul.nh.shaon@gmail.com>
2022-10-02 20:45:36 +0600

    calculate total expense added

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('worked');
+document.getElementById('expense-calculate').addEventListener('click', function () {
+    const foodCostElement = document.getElementById('food-cost');
+    const rentCostElement = document.getElementById('rent-cost');
+    const clothsCostElement = document.getElementById('cloths-cost');
+    const totalExpenseElement = document.getElementById('total-expense');
+
+    const foodCostString = foodCostElement.value;
+    const rentCostString = rentCostElement.value;
+    const clothsCostString = clothsCostElement.value;
+
+    const foodCost = parseInt(foodCostString);
+    const rentCost = parseInt(rentCostString);
+    const clothsCost = parseInt(clothsCostString);
+
+    const totalExpense = foodCost + rentCost + clothsCost;
+
+    totalExpenseElement.innerText = totalExpense;
+
+    // calculate balance 
+
+
+
+
+
+    console.log(totalExpense);
+})
